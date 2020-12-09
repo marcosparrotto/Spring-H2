cad1ae3672959fcbfa6a7e964461232191f25ba2
Marcos Parrotto <marcosparrotto@gmail.com>
2020-12-09 14:10:41 -0300

    OrderStatus enum

--- src/main/java/com/marcos/spring/entities/Order.java
+++ src/main/java/com/marcos/spring/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.marcos.spring.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -25,6 +26,7 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern ="yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name="client_id")
@@ -34,9 +36,10 @@ public class Order implements Serializable{
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		this.orderStatus = orderStatus.getCode();
 		this.client = client;
 	}
 
@@ -62,6 +65,14 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus.getCode();
 	}
 
 	@Override
